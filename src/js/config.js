89b4c44378a313ce81b6e16de416eeae115ba393
Hyeeun Mok <hyeeuni.mok@gmail.com>
2021-04-16 22:30:56 -0400

    uploading new recipe - p1 and p2.5

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,4 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '0763be37-ba95-48d8-bb72-a1d75fe8a7d7';
